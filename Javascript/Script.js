adc5ebd664688862b8b51a47d40c9229b2afbdf0
Korpo-spec <60465693+Korpo-spec@users.noreply.github.com>
2020-05-20 12:49:52 +0200

    Animations working

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -4,85 +4,79 @@ menuButton.addEventListener("click", MenubuttonClicked);
 let animationDirection = ["normal", "reverse"];
 let animationIndex = false;
 
+
+
 const elem = document.querySelector(".dropDownMenu");
-const elem2 = document.getElementById("dropDownMenu2")
+
+const gridElems = document.querySelectorAll(".gridItem");
+const elem2 = gridElems[1];
+const buttons = document.querySelectorAll(".buttonClass");
+
+
+for (let index = 1; index < gridElems.length; index++) {
+    
+    gridElems[index].style.display = "none";
+    
+}
+
+
+console.log(buttons.length)
+for (let index = 0; index < buttons.length; index++) {
+    const element = buttons[index];
+    buttons[index].addEventListener("click", buttonClicked);
+    
+}
+
+let firstDone = true;
+let currentPage = elem;
+document.querySelector(".buttonClass").addEventListener("click", buttonClicked);
+function buttonClicked() {
+    let number = parseInt(this.value, 10)  + 1;
+    gridElems[number].style.display = "block";
+    animateObjectHorizontally(gridElems[1], 0, window.innerWidth, 1000);
+    animateObjectHorizontally(gridElems[number], -window.innerWidth, 0, 1000);
+    animationIndex = !animationIndex;
+    currentPage = gridElems[number];
+}
 
 let timesClicked = 0;
-/*
-elem.animate([
-    {
-        clipPath: "circle(100%)",
-    },
-    {
-        clipPath: "circle(0%)",
-    }
-], 1000).onfinish = function(){
 
-    elem.style.display = "none";
-    elem2.style.display= "block";
-    elem2.animate([
-    {
-        clipPath: "circle(0%)",
-    },
-    {
-        clipPath: "circle(100%)",
-    }
-], 1000)
-}
-*/
 function MenubuttonClicked() {
-    /*
-    document.querySelector(".dropDownMenu").style.animationPlayState = "paused";
-    document.querySelector(".dropDownMenu").style.animationDirection = animationDirection[animationIndex];
-    document.querySelector(".dropDownMenu").style.animationPlayState = "running";
-    
-    if (animationIndex == 0) {
-        animationIndex = 1
-    }
-    else{
-        animationIndex = 0
-    }
-    
-
-   document.querySelector(".dropDownMenu").animate([
-    {transform: 'translateX(0%)'},
-    {transform: 'translateX(100%)'}
-    ],{
-        duration:1000,
-        iterations: 1,
-        fill: "forwards"
-    })
-
-    */
-
     animationIndex = !animationIndex;
 
     if (animationIndex) {
-        elem.style.display = "block";
+        currentPage.style.display = "block";
+
+        elem2.style.transform = 'translateY('+-window.innerHeight+'px)';
         elem2.style.display = "block";
-        animateObject(elem, 0, window.innerHeight, 1000);
-        animateObject(elem2, -window.innerHeight, 0, 1000);
+
+        animateObjectVertically(currentPage, 0, window.innerHeight, 800);
+        animateObjectVertically(elem2, -window.innerHeight, 0, 800);
 
         timesClicked++;
 
     }
     else{
-        elem.style.display = "block";
+        currentPage.style.display = "block";
         elem2.style.display = "block";
-        animateObject(elem, window.innerHeight, 0, 1000);
-        animateObject(elem2, 0, -window.innerHeight, 1000);
+        animateObjectVertically(currentPage, window.innerHeight, 0, 800);
+        animateObjectVertically(elem2, 0, -window.innerHeight, 800);
         timesClicked++;
     }
    
 }
-function animateObject(element, start, stop, time) {
+function animateObjectVertically(element, start, stop, time) {
     
     start = parseFloat(getComputedStyle(element).transform.split(',')[5]) || 0;
 
     element.animate([
     {transform: 'translateY('+start+'px)'},
     {transform: 'translateY('+stop+'px)'}
-    ],time).onfinish = function(){
+    ],
+    {
+        duration: time,
+        easing: "cubic-bezier(0.42, 0, 0.58, 1)"
+    }).onfinish = function(){
         // block the element transition to the '_stop' position
         element.style.transform = 'translateY('+stop+'px)';
         timesClicked--;
@@ -94,6 +88,24 @@ function animateObject(element, start, stop, time) {
         }
     }
 
-   
+}
+function animateObjectHorizontally(element, start, stop, time) {
+    
+
+    element.animate([
+        {transform: 'translateX('+start+'px)'},
+        {transform: 'translateX('+stop+'px)'}
+        ],
+        {
+            duration: time,
+            easing: "cubic-bezier(0.42, 0, 0.58, 1)"
+        }).onfinish = function(){
+            if (firstDone) {
+                
+                element.style.display = "none"
+            }
+            firstDone = !firstDone;
+            
+        }
 }
 
